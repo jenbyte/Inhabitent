05c70bd520776c4bc24ca1701df778a200483094
Jennifer <jen.yiu.90@gmail.com>
2018-11-09 13:31:55 -0800

    first commit

---
+++ themes/tent/js/custom.js
@@ -0,0 +1,8 @@
+(function($) {
+  $(document).ready(function() {
+    // your code here
+    $('body').append('hello');
+
+    // TODO add search form toggle - .focus and .blur methods
+  });
+})(jQuery);
