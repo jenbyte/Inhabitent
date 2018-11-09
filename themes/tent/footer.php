a85ff2bddafe8ee33c8ecfed6d20fd7a9c73e67b
Jennifer <jen.yiu.90@gmail.com>
2018-11-09 14:58:08 -0800

    footer, font and color variables, mixin

--- themes/tent/footer.php
+++ themes/tent/footer.php
@@ -7,14 +7,34 @@
 
 ?>
 
-			</div><!-- #content -->
+	</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<div class="footer-block-item">
+				<h3>contact</h3>
+				<p>info@inhabitent.com</p>
+				<p>778-456-7891</p>
+				<p>
+					<span><i class="fa fa-facebook-square"></i></span>
+					<span><i class="fa fa-twitter-square"></i></span>
+					<span><i class="fa fa-google-plus-square"></i></span>
+				</p>
+			</div>
+			<div class="footer-block-item">
+				<h3>business hours</h3>
+				<p>Monday-Friday: 9am to 5pm</p>
+				<p>Saturday: 10am to 2pm</p>
+				<p>Sunday: Closed</p>
+			</div>
+			<div class="text-logo">
+				<img src="../../images/inhabitent-logo-text-dark.svg" alt='Inhabitent Logo'>
+			</div>
+			<!-- <a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a> -->
+		</div><!-- .site-info -->
+		<div class="copyright">copyright &copy; 2019 inhabitent</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
 
 		<?php wp_footer(); ?>
 
--- themes/tent/js/custom.js
+++ themes/tent/js/custom.js
@@ -1,8 +1,7 @@
 (function($) {
   $(document).ready(function() {
     // your code here
-    $('body').append('hello');
-
+    // $('body').append('hello');
     // TODO add search form toggle - .focus and .blur methods
   });
 })(jQuery);
