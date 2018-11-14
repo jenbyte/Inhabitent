df6ed2b32f882f9b254294804e4524b5cda6e935
Jennifer <jen.yiu.90@gmail.com>
2018-11-13 20:09:39 -0800

    position of text-logo, style sidebar, edit footer html

--- themes/tent/footer.php
+++ themes/tent/footer.php
@@ -13,20 +13,24 @@
 		<div class="container">
 		<div class="site-info">
 			<div class="footer-block-item">
-				<h3>contact</h3>
-				<p>info@inhabitent.com</p>
-				<p>778-456-7891</p>
+				<div class="contact">
+					<h3>contact</h3>
+					<p><a href="mailto: info@inhabitent.com"><i class="fa fa-envelope"></i> info@inhabitent.com</a></p>
+					<p><a href="tel:5553434567891"><i class="fa fa-mobile-alt"></i> 778-456-7891</a></p>
 				<p>
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-twitter-square"></i></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
 				</p>
+				</div>
 			</div>
 			<div class="footer-block-item">
-				<h3>business hours</h3>
-				<p>Monday-Friday: 9am to 5pm</p>
-				<p>Saturday: 10am to 2pm</p>
-				<p>Sunday: Closed</p>
+				<div class="hours">
+					<h3>business hours</h3>
+				<p><span class="days-of-week">Monday-Friday:</span> 9am to 5pm</p>
+				<p><span class="days-of-week">Saturday:</span> 10am to 2pm</p>
+				<p><span class="days-of-week">Sunday:</span> Closed</p>
+				</div>
 			</div>
 			<div class="footer-block-item">
 				<div class="text-logo">
